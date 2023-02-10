e6848d5cebaca4d38e60f0e8fc635890e83a7eab
Victor Alves <victoralvssalcantara@gmail.com>
2023-02-09 22:30:49 -0300

    Updating elements according to its semantic and acessable use

--- Junior/dark-theme-landing-page/src/script.js
+++ Junior/dark-theme-landing-page/src/script.js
@@ -7,8 +7,6 @@ sendEmail.addEventListener("click", (e) => {
   const regex = /^([\w\.-]+)@([a-z0-9]+)\.([a-z]+)(\.[A-Z]+)*$/i;
   const email = userEmail.value;
 
-  window.alert("asass");
-
   if (!regex.test(email)) {
     errorMessage.classList.add("active");
     errorMessage.textContent = "Please enter a valid email address";
